45f3a9673b8dd617372a017719ce22d798b5593e
pfvo <pedrofvoliveira94@gmail.com>
2022-03-25 21:40:50 +0000

    adding db url fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
     }
   });
